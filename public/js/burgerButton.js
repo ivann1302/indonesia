c7067310bf8dabebe35d189bd71f01917543d7b4
ivan <ivann97n@gmail.com>
2024-12-18 14:43:25 +0300

    add toggle for burger

--- public/js/burgerButton.js
+++ public/js/burgerButton.js
@@ -1,7 +1,31 @@
-const burger = document.querySelector('.header__burger-menu');
+const burgerCheckbox = document.querySelector('#burger-checkbox');
+const list = document.querySelector('.burger-menu__list');
 
- burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
+burgerCheckbox.addEventListener('change', () => {
+  // Нет необходимости в JavaScript для управления состоянием, всё контролируется через чекбокс и CSS
+  if (burgerCheckbox.checked) {
+    list.style.transform = 'translateX(-100%)';
+    list.style.display = 'grid';
+  } else {
+    list.style.display = 'none';
+  }
 });
 
-export default burger;
+export default burgerCheckbox;
+
+
+/**
+ *  const text = parentContent?.querySelector('.steps__description');
+
+        if (text) {
+            // Переключаем класс `show` для отображения полного текста
+            text.classList.toggle('show');
+
+            // Меняем текст кнопки
+            this.textContent = text.classList.contains('show') ? 'Show Less' : 'Show More';
+        } else {
+            console.warn('No .steps__description found for this button');
+        }
+    });
+});
+ */
